526ea3fe73fa9ef7822eda9ee9c322b22ee14116
Eric Johnson <ejohn156@uncc.edu>
2018-10-17 19:39:34 -0400

    connected navbar, footer, sidebar components to app and imported bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Sidebar from './components/SideBar'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      //<div className="App">
+      <div class="container">
+      <div class="row">
+      <div class="col-md-12">
+      <Navbar />
+      </div>
+      </div>
+      <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-3">
+      <Sidebar />
+      </div>
+      <div class="col-md-7">
+      <h1>Content of page</h1>
+      </div>
+      </div>
+      <div class="row">
+      <div class="col-md-12">
+      <Footer />
+      </div>
+      </div>
+      </div>
+      /*
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -22,6 +48,7 @@ class App extends Component {
           </a>
         </header>
       </div>
+      */
     );
   }
 }
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Navbar = props => (
+    //html goes here to design and create navbar
+  <h1>Navbar</h1>
+);
+
+export default Navbar;
