1fb0b1a417deac1e715c15a167f89f4060e84fc6
Eric Johnson <ejohn156@uncc.edu>
2018-10-23 17:30:41 -0400

    added navbar active, browse job/services

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import browsePage from './pages/browsePage';
+import browseJobPage from './pages/browseJobPage';
+import browseServicePage from './pages/browseServicePage'
 import profilePage from './pages/profilePage';
 import homePage from './pages/homePage';
 import instructionPage from './pages/instructionPage';
 import authPage from './pages/authPage';
+import postPage from './pages/PostPage'
 
 
 class App extends Component {
+  state = {
+    currentPage: "home"
+  }
   render() {
     return (
       
     <div className="Container">
-    <div class="row">
-    <div class="col-md-12">
-    <Navbar />
-    </div>
-    </div>
-    <div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-11">
+
     <Router>
           <Switch>
-
-            <Route exact path="/" render={() => (
-              true ? (
-                <Redirect to="/home"/>
-              ) : (
-                  <Redirect to="/Authentication" />
-                )
-            )} />
-            {/* <Route exact path="/" component={LoginPage} /> */}
-            <Route exact path="/home" component={homePage} />
-            <Route exact path="/instruction" component={instructionPage} />
+            <Route exact path="/" component={authPage}/>
+            <Route path="/home" component={homePage}/>
+            <Route exact path="/instruction" component={instructionPage}  />
             <Route path="/profile" component={profilePage} />
-            <Route path="/browse" component={browsePage}/>
+            <Route path="/browse/job" component={browseJobPage}/>
+            <Route path="/browse/service" component={browseServicePage}/>
             <Route exact path="/Authentication" component={authPage}/>
+            <Route exact path="/post" component={postPage}/>
           </Switch>
           </Router>
-        </div>
-        </div>
         <div class="row">
         <div class="col-md-12">
       <Footer />
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,32 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Navbar = props => (
+export default class Navbar extends Component {
     //html goes here to design and create navbar
+    state = {
+      activePage: this.props.page
+    }
+    render(){
+      return(
+    <div class="row">
+    <div class="col-md-12">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">The Quarry</a>
+    <a class="navbar-brand" href="/">The Quarry</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item active">
+        <li class={this.state.activePage === "home" ? "nav-item active":"nav-item"}>
           <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/browse">browse</a>
+        <li class={this.state.activePage === "browseJob" ? "nav-item active":"nav-item"}>
+          <a class="nav-link" href="/browse/job">Browse Jobs</a>
         </li>
-        <li class="nav-item">
+        <li class={this.state.activePage === "browseService" ? "nav-item active":"nav-item"}>
+          <a class="nav-link" href="/browse/service">Browse Services</a>
+        </li>
+        <li class={this.state.activePage === "profile" ? "nav-item active":"nav-item"}>
           <a class="nav-link" href="/profile">Profile</a>
         </li>
-        <li class="nav-item">
+        <li class={this.state.activePage === "instructions" ? "nav-item active":"nav-item"}>
           <a class="nav-link" href="/instruction">Instructions</a>
         </li>
-        <li class="nav-item">
+        <li class={this.state.activePage === "authentication" ? "nav-item active":"nav-item"}>
           <a class="nav-link" href="/authentication">Login/Sign-up</a>
+        </li>
+        <li class={this.state.activePage === "post" ? "nav-item active":"nav-item"}>
+          <a class="nav-link" href="/post">Post a Job/Service</a>
         </li>
       </ul>
     </div>
   </nav>
+  </div>
+  </div>
 );
-
-export default Navbar;
+}
+}
