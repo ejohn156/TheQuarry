8bbdfe9e927b5f5c7e84cc6f011d6de41544f4c2
Chris Janiszewski <cjanisze@uncc.edu>
2018-10-31 15:45:14 -0400

    Added more to add job page

--- client/src/pages/PostPage/post.js
+++ client/src/pages/PostPage/post.js
@@ -21,7 +21,27 @@ const postPage = ({ match }) => (
     </div>
       <div class="form-group">
         <label for="JobName">Name of Job</label>
-        <textarea class="form-control" id="Job Name" rows="1"></textarea>
+        <textarea type ="text" class="form-control" id="Job Name" rows="1" placeholder="Job Name"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="Jobdescription">Job Description</label>
+        <textarea type="text" class="form-control" id="Job Description" rows="4" placeholder="Job Description"></textarea> 
+      </div>
+
+      <form class="form-inline">
+  <label class="mr-sm-2" for="inlineFormCustomSelect">Category</label>
+  <select class="custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
+    <option selected>Choose...</option>
+    <option value="1">Technology</option>
+    <option value="2">Artist</option>
+    <option value="3">Mechanical</option>
+    <option value="4">Academic</option>
+  </select>
+  </form>
+
+      <div class="form-group">
+        <label for="Jobdescription">Necessary Skills</label>
+        <textarea type="text" class="form-control" id="Necessaryskills" rows="4" placeholder="Necessary Skills"></textarea> 
       </div>
       </div>
       </div>
