d0c2f48937c9ef09ac5c89cb3455036ed51f8c98
Josh <jgratton24@gmail.com>
2018-11-15 14:12:59 -0500

    Created FAQ page. Still a WIP

--- client/src/pages/instructionPage/instruction.js
+++ client/src/pages/instructionPage/instruction.js
@@ -6,13 +6,66 @@ const instructionPage = ({ match }) => (
 
   <div>
     <Navbar page="instructions"/>
-    <div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-11">
-    <h1>Instruction page</h1>
+  
+    <div class="wrapper">
+<div class="strong">
+  <h1><strong>Frequently Asked Questions</strong></h1>
+</div>
+  <br></br>
+  <div class="half">
+    <div class="tab">
+      <input id="tab-one" type="checkbox" name="tabs"/>
+      <label for="tab-one">Question #1</label>
+      <div class="tab-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+      </div>
     </div>
+    <div class="tab">
+      <input id="tab-two" type="checkbox" name="tabs"/>
+      <label for="tab-two">Question #2</label>
+      <div class="tab-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+      </div>
     </div>
+    <div class="tab">
+      <input id="tab-three" type="checkbox" name="tabs"/>
+      <label for="tab-three">Question #3</label>
+      <div class="tab-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+      </div>
     </div>
+    <div class="tab">
+      <input id="tab-two" type="checkbox" name="tabs"/>
+      <label for="tab-two">Question #4</label>
+      <div class="tab-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+      </div>
+    </div>
+    <div class="tab">
+      <input id="tab-two" type="checkbox" name="tabs"/>
+      <label for="tab-two">Question #5</label>
+      <div class="tab-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+      </div>
+    </div>
+    <div class="tab">
+      <input id="tab-two" type="checkbox" name="tabs"/>
+      <label for="tab-two">Question #6</label>
+      <div class="tab-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+      </div>
+    </div>
+    <br></br>
+  </div>
+  
+ 
+</div>
+    
+
+ 
+
+  </div>
+
 
 
 );
