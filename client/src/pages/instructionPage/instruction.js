2dc0178845cf999c9b3c190c926b083f5c089430
Kishan <kkalari1@uncc.edu>
2018-11-16 14:03:31 -0500

    FQA adds

--- client/src/pages/instructionPage/instruction.js
+++ client/src/pages/instructionPage/instruction.js
@@ -12,7 +12,7 @@ const instructionPage = ({ match }) => (
   <br></br>
   <div class="half">
     <div class="tab">
-      <input id="tab-one" type="checkbox" name="tabs"/>
+     <input id="tab-one" type="checkbox" name="tabs"/>
      <label for="tab-one">What is a <i>service</i> defined as?</label>
       <div class="tab-content">
         <p>A <i>service</i> is something a student wants to provide or offer to others.</p>
@@ -53,6 +53,25 @@ const instructionPage = ({ match }) => (
         <p>If your account is temporarily banned there is no appeal process. However, if your account is temporarily banned you may submit an appeal.</p>
       </div>
     </div>
+    <div class="tab">
+     <input id="tab-seven" type="checkbox" name="tabs"/>
+     <label for="tab-seven">How do I advertise a <i>job</i> or <i>service</i>?</label>
+      <div class="tab-content">
+        <p>Once you sign up and log in you will be directed to <i>service</i> and <i>job</i> form you can fill out.</p>
+      </div>
+    </div>
+    <div class="tab">
+     <input id="tab-eight" type="checkbox" name="tabs"/>
+     <label for="tab-eight">How do I search for help? </label>
+      <div class="tab-content">
+        <p>Simply click on the <i>browse</i> tab and search for help depending on whether you need help with <i>job</i> or <i>service</i>.</p>
+      </div>
+    </div>
+
+
+   
+
+
     <br></br>
     <br></br>
     <br></br>
