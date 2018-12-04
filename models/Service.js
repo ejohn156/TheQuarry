47ca4ceb559531fb742b2d3834bd9ec507f8c3b8
Eric Johnson <ejohn156@uncc.edu>
2018-12-04 16:16:45 -0500

    set up so browse will only show jobs/services others have posted

--- models/Service.js
+++ models/Service.js
@@ -5,6 +5,7 @@ const serviceSchema = new Schema({
     title: {type: String,required: true,unique: false},
     description: {type: String,required: true,unique: false},
     creator: {type: String,required: true,unique: false},
+    creatorID: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile', required: true, unique: false},
     category:{type: String,required: true,unique: false},
     date: { type: Date, default: Date.now, unique: false },
     hourly:{type: Number,require: false,unique: false}
